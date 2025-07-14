734b3d67f85c70d41b721285590163d7c75ce8fc
Isaac Rudnick <48895941+IsaacFigNewton@users.noreply.github.com>
2025-07-13 22:19:46 -0700

    added pkg template files

---
+++ src/noske/__init__.py
@@ -0,0 +1,16 @@
+
+__version__ = '1.0.1'
+__authors__ = 'Isaac Rudnick'
+
+"""
+
+"""
+
+# Import stuff from the package
+
+
+# Define what gets imported with "from package import *"
+__all__ = [
+    # Main classes
+    
+]
