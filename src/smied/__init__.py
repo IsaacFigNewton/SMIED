b781aa1cee9e06e42fac365c5193a0d1c4355300
Isaac <48895941+IsaacFigNewton@users.noreply.github.com>
2025-10-19 15:38:21 -0700

    Update __init__.py
    re-running tests

--- src/smied/__init__.py
+++ src/smied/__init__.py
@@ -4,8 +4,6 @@ from .DirectedMetagraph import DirectedMetagraph
 from .SemanticMetagraph import SemanticMetagraph
 from .PatternLoader import PatternLoader
 from .PatternMatcher import PatternMatcher
-from .FramenetSpacySRL import FrameNetSpaCySRL
-from .SemanticDecomposer import SemanticDecomposer
 
 # Define what gets imported with "from package import *"
 __all__ = [
@@ -14,6 +12,4 @@ __all__ = [
     "SemanticMetagraph",
     "PatternLoader",
     "PatternMatcher",
-    "FrameNetSpaCySRL",
-    "SemanticDecomposer",
 ]
